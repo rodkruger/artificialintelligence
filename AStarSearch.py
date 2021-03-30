09a6f32b43d17296c1648b4f7998c01a9b4e9bf8
Rodrigo Kruger <rkruger@fh.com.br>
2021-03-29 22:11:36 -0300

    Adicionado comentários ao código

--- AStarSearch.py
+++ AStarSearch.py
@@ -1,185 +1,212 @@
-# This class represent a node
-class Node:
+# Definição de um nó em cada grafo
+class Nodo:
 
-    # Initialize the class
-    def __init__(self, name: str, parent: str):
-        self.name = name
-        self.parent = parent
-        self.g = 0  # Distance to start node
-        self.h = 0  # Distance to goal node
-        self.f = 0  # Total cost
+    # Construtor principal
+    def __init__(self, name: str, pai: str):
+        self.nome = name
+        self.pai = pai
+        self.g = 0          # Distância para o nó inicial
+        self.h = 0          # Distância para o objetivo
+        self.f = 0          # Custo total
 
-    # Compare nodes
+    # Sobrescrevendo o método que testa se dois objetos são iguais.
+    # Serão se eles possuírem o mesmo nome
     def __eq__(self, other):
-        return self.name == other.name
+        return self.nome == other.nome
 
-    # Sort nodes
+    # Ordenar os nós. O método os colocará por ordem de custo total (crescente)
     def __lt__(self, other):
         return self.f < other.f
 
-    # Print node
+    # Imprime informações sobre o nó no console
     def __repr__(self):
-        return ('({0},{1})'.format(self.name, self.f))
+        return ('({0},{1})'.format(self.nome, self.f))
 
 
-# This class represent a graph
-class Graph:
+# Representação de um grafo convencional, direcionado ou não direcionado
+class Grafo:
 
-    # Initialize the class
-    def __init__(self, graph_dict=None, directed=True):
-        self.graph_dict = graph_dict or {}
-        self.directed = directed
-        if not directed:
-            self.make_undirected()
+    # Construtor principal
+    def __init__(self, grafo_dic=None, direcionado=True):
+        self.grafo_dic = grafo_dic or {}
+        self.direcionado = direcionado
+        if not direcionado:
+            self.tornar_não_direcionado()
 
-    # Create an undirected graph by adding symmetric edges
-    def make_undirected(self):
-        for a in list(self.graph_dict.keys()):
-            for (b, dist) in self.graph_dict[a].items():
-                self.graph_dict.setdefault(b, {})[a] = dist
+    # Criar um grafo não-direcionado, adicionando nós paralelos simétricos
+    # ao nó principal
+    def tornar_não_direcionado(self):
+        for a in list(self.grafo_dic.keys()):
+            for (b, dist) in self.grafo_dic[a].items():
+                self.grafo_dic.setdefault(b, {})[a] = dist
 
-    # Add a link from A and B of given distance, and also add the inverse link if the graph is undirected
-    def connect(self, A, B, distance=1):
-        self.graph_dict.setdefault(A, {})[B] = distance
-        if not self.directed:
-            self.graph_dict.setdefault(B, {})[A] = distance
+    # Adiciona uma ligação de A para B de uma certa distância, e também
+    # adiciona uma ligação inversa se o grafo for não direcionado
+    def conectar(self, A, B, distancia=1):
+        self.grafo_dic.setdefault(A, {})[B] = distancia
+        if not self.direcionado:
+            self.grafo_dic.setdefault(B, {})[A] = distancia
 
-    # Get neighbors or a neighbor
-    def get(self, a, b=None):
-        links = self.graph_dict.setdefault(a, {})
+    # Obter os vizinhos ou um vizinho
+    def obter(self, a, b=None):
+        links = self.grafo_dic.setdefault(a, {})
         if b is None:
             return links
         else:
             return links.get(b)
 
-    # Return a list of nodes in the graph
-    def nodes(self):
-        s1 = set([k for k in self.graph_dict.keys()])
-        s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
-        nodes = s1.union(s2)
-        return list(nodes)
+    # Retorna uma lista de nós presentes no grafo
+    def nodos(self):
+        s1 = set([k for k in self.grafo_dic.keys()])
+        s2 = set([k2 for v in self.grafo_dic.values() for k2, v2 in v.items()])
+        nodos = s1.union(s2)
+        return list(nodos)
 
 
-# A* search
-def astar_search(graph, heuristics, start, end):
+# Busca A* (A-Estrela). Utiliza busca em largura, somada a uma heurística baseada
+# na distância ao objetivo final
+def busca_aestrela(grafo, heuristicas, inicio, final):
 
-    # Create lists for open nodes and closed nodes
-    open = []
-    closed = []
+    # Lista de nós abertos e fechados, que são utilizados para controle de
+    # visitação sobre o grafo
+    abertos = []
+    fechados = []
 
-    # Create a start node and an goal node
-    start_node = Node(start, None)
-    goal_node = Node(end, None)
+    # Estabelecer um nó inicial e final para o grafo. Obviamente o início aponta para o
+    # ponto de partida para a busca, e o nó final o objetivo
+    no_inicial = Nodo(inicio, None)
+    no_objetivo = Nodo(final, None)
 
-    # Add the start node
-    open.append(start_node)
+    # Adiciona o nó inicial na lista de nós abertos. Esta lista controla os nós ainda
+    # não visitados
+    abertos.append(no_inicial)
 
-    # Loop until the open list is empty
-    while len(open) > 0:
+    # Iteragir sobre a lista de nós abertos, até que todos tenham sido visitados pelo
+    # menos uma vez
+    while len(abertos) > 0:
 
-        # Sort the open list to get the node with the lowest cost first
-        open.sort()
+        # Ordenar a lista de nós abertos para sempre visitar o próximo nó mais próximo
+        # do objetivo final. Lembrando que a ordenação dos grafos ocorre pelo custo total
+        # até o objetivo !!
+        abertos.sort()
 
-        # Get the node with the lowest cost
-        current_node = open.pop(0)
+        # Uma vez ordenada a lista, no início dela teremos o nó de menor custo. Pegamos ele
+        # e faremos a próxima visita sobre ele
+        no_corrente = abertos.pop(0)
 
-        # Add the current node to the closed list
-        closed.append(current_node)
+        # Adiciona o nó corrente na lista de nós fechados ou visitados
+        fechados.append(no_corrente)
 
-        # Check if we have reached the goal, return the path
-        if current_node == goal_node:
-            path = []
-            while current_node != start_node:
-                path.append(current_node.name + ': ' + str(current_node.g))
-                current_node = current_node.parent
-            path.append(start_node.name + ': ' + str(start_node.g))
-            # Return reversed path
-            return path[::-1]
+        # Se o nó corrente é o objetivo, então a busca localizou o nó e o seu caminho
+        # mais próximo. No loop abaixo é feito o caminho do nó corrente (e objetivo final)
+        # até o seu pai
+        if no_corrente == no_objetivo:
+            caminho = []
+            while no_corrente != no_inicial:
+                caminho.append(no_corrente.nome + ': ' + str(no_corrente.g))
+                no_corrente = no_corrente.pai
+            caminho.append(no_inicial.nome + ': ' + str(no_inicial.g))
+            # Retorna o caminho na ordem inversa ... pois, no loop acima, é feito um caminho
+            # do objetivo ao início. Mas, queremos o caminho do início ao objetivo, :)
+            return caminho[::-1]
 
-        # Get neighbours
-        neighbors = graph.get(current_node.name)
+        # Se chegou até aqui, é pq não atingimos o objetivo final !
 
-        # Loop neighbors
-        for key, value in neighbors.items():
+        # Vamos obter os vizinhos do nó corrente, e fazer uma visitação em cada um, sempre
+        # respeitando a heurística da distância total ao objetivo
+        vizinhos = grafo.obter(no_corrente.nome)
 
-            # Create a neighbor node
-            neighbor = Node(key, current_node)
+        # Iteragir sobre todos os vizinhos do nó atual
+        for chave, valor in vizinhos.items():
 
-            # Check if the neighbor is in the closed list
-            if (neighbor in closed):
+            # Criar um nó vizinho, a partir da chave correntamente selecionado
+            vizinho = Nodo(chave, no_corrente)
+
+            # Se o vizinho selecionado já foi visitado, ele não é mais considerado !
+            if (vizinho in fechados):
                 continue
 
-            # Calculate full path cost
-            neighbor.g = current_node.g + graph.get(current_node.name, neighbor.name)
-            neighbor.h = heuristics.get(neighbor.name)
-            neighbor.f = neighbor.g + neighbor.h
+            # Calcular o custo total do caminho. Na tabela de heurísticas temos a distância
+            # total em linha reta do nó atual para o objetivo
+            vizinho.g = no_corrente.g + grafo.obter(no_corrente.nome, vizinho.nome)
+            vizinho.h = heuristicas.get(vizinho.nome)
+            vizinho.f = vizinho.g + vizinho.h
 
-            # Check if neighbor is in open list and if it has a lower f value
-            if (add_to_open(open, neighbor) == True):
+            # Verificar se o vizinho possui custo menor que o atual, e não está na lista
+            # de nós abertos
+            if (adicionar_aberto(abertos, vizinho) == True):
 
                 # Everything is green, add neighbor to open list
-                open.append(neighbor)
+                abertos.append(vizinho)
 
-    # Return None, no path is found
+    # Retorna vazio. Ou seja, nenhum caminho encontrado
     return None
 
 
-# Check if a neighbor should be added to open list
-def add_to_open(open, neighbor):
-    for node in open:
-        if (neighbor == node and neighbor.f > node.f):
+# Adiciona se o vizinho deve ser visitado ou não. Note que o IF testa se o caminho deste
+# vizinho possui distância total maior que o nó atual, este método retorna Falso ...
+def adicionar_aberto(abertos, vizinho):
+    for node in abertos:
+        if (vizinho == node and vizinho.f > node.f):
             return False
     return True
 
 
-# The main entry point for this module
+#########################################################################################
+
 def main():
-    # Create a graph
-    graph = Graph()
-    # Create graph connections (Actual distance)
-    graph.connect('Curitiba', 'Joinville', 120)
-    graph.connect('Joinville', 'Itajaí', 93)
-    graph.connect('Itajaí', 'Florianópolis', 101)
-    graph.connect('Florianópolis', 'Criciúma', 207)
-    graph.connect('Criciúma', 'Torres', 104)
-    graph.connect('Torres', 'Osório', 95)
-    graph.connect('Osório', 'Porto Alegre', 104)
+    # Criar o grafo
+    grafo = Grafo()
 
-    graph.connect('Curitiba', 'Mafra', 116)
-    graph.connect('Mafra', 'Papanduva', 53)
-    graph.connect('Papanduva', 'Santa Cecília', 90)
-    graph.connect('Santa Cecília', 'Ponte Alta', 68)
-    graph.connect('Ponte Alta', 'Lages', 42)
-    graph.connect('Lages', 'Vacaria', 108)
-    graph.connect('Vacaria', 'Caxias do Sul', 112)
-    graph.connect('Caxias do Sul', 'Novo Hamburgo', 97)
-    graph.connect('Novo Hamburgo', 'Porto Alegre', 50)
+    # Criar as cidades, suas conexões e suas distâncias
 
-    # Make graph undirected, create symmetric connections
-    graph.make_undirected()
-    # Create heuristics (straight-line distance, air-travel distance)
-    heuristics = {}
-    heuristics['Joinville'] = 622
-    heuristics['Itajaí'] = 536
-    heuristics['Florianópolis'] = 462
-    heuristics['Criciúma'] = 285
-    heuristics['Torres'] = 193
-    heuristics['Osório'] = 104
-    heuristics['Mafra'] = 586
-    heuristics['Papanduva'] = 538
-    heuristics['Santa Cecília'] = 449
-    heuristics['Ponte Alta'] = 380
-    heuristics['Lages'] = 343
-    heuristics['Vacaria'] = 239
-    heuristics['Caxias do Sul'] = 127
-    heuristics['Novo Hamburgo'] = 49
-    heuristics['Porto Alegre'] = 0
+    grafo.conectar('Curitiba', 'Joinville', 120)
+    grafo.conectar('Joinville', 'Itajaí', 93)
+    grafo.conectar('Itajaí', 'Florianópolis', 101)
+    grafo.conectar('Florianópolis', 'Criciúma', 207)
+    grafo.conectar('Criciúma', 'Torres', 104)
+    grafo.conectar('Torres', 'Osório', 95)
+    grafo.conectar('Osório', 'Porto Alegre', 104)
 
-    # Run the search algorithm
-    path = astar_search(graph, heuristics, 'Curitiba', 'Porto Alegre')
+    grafo.conectar('Curitiba', 'Mafra', 116)
+    grafo.conectar('Mafra', 'Papanduva', 53)
+    grafo.conectar('Papanduva', 'Santa Cecília', 90)
+    grafo.conectar('Santa Cecília', 'Ponte Alta', 68)
+    grafo.conectar('Ponte Alta', 'Lages', 42)
+    grafo.conectar('Lages', 'Vacaria', 108)
+    grafo.conectar('Vacaria', 'Caxias do Sul', 112)
+    grafo.conectar('Caxias do Sul', 'Novo Hamburgo', 97)
+    grafo.conectar('Novo Hamburgo', 'Porto Alegre', 50)
+
+    # O grafo em questão é não direcionado !
+
+    grafo.tornar_não_direcionado()
+
+    # Criação da heurística. Neste caso, a heurística é a distância em linha reta para
+    # o objetivo final
+    heuristicas = {}
+    heuristicas['Joinville'] = 622
+    heuristicas['Itajaí'] = 536
+    heuristicas['Florianópolis'] = 462
+    heuristicas['Criciúma'] = 285
+    heuristicas['Torres'] = 193
+    heuristicas['Osório'] = 104
+    heuristicas['Mafra'] = 586
+    heuristicas['Papanduva'] = 538
+    heuristicas['Santa Cecília'] = 449
+    heuristicas['Ponte Alta'] = 380
+    heuristicas['Lages'] = 343
+    heuristicas['Vacaria'] = 239
+    heuristicas['Caxias do Sul'] = 127
+    heuristicas['Novo Hamburgo'] = 49
+    heuristicas['Porto Alegre'] = 0
+
+    # Executar a busca A*
+
+    path = busca_aestrela(grafo, heuristicas, 'Curitiba', 'Porto Alegre')
     print(path)
     print()
 
 
-# Tell python to run main method
+# Rodar o método Main, :)
 if __name__ == "__main__": main()
